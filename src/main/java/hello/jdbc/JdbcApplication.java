4098c85aea24bea4b34a820c7742818a7b9fe4cb
cas90 <qkqhemforhs1@gamail.com>
2022-10-30 20:13:35 +0900

    Spring-BOOT
    H2 DB 생성

--- src/main/java/hello/jdbc/JdbcApplication.java
+++ src/main/java/hello/jdbc/JdbcApplication.java
@@ -3,14 +3,20 @@ package hello.jdbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class JdbcApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args, List list) {
 		SpringApplication.run(JdbcApplication.class, args);
+
+
+
+
 	}
 
 
-	ㅋㅋ;
+
 
 }
