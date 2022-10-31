3deff98199095977e0efe1b0e2f92ab9de9f2db1
Kimminwoo6039 <qkqhemforhs1@gmail.com>
2022-11-01 06:35:17 +0900

    H2 DB 초기설정

--- src/main/java/hello/jdbc/JdbcApplication.java
+++ src/main/java/hello/jdbc/JdbcApplication.java
@@ -8,7 +8,7 @@ import java.util.List;
 @SpringBootApplication
 public class JdbcApplication {
 
-	public static void main(String[] args, List list) {
+	public static void main(String[] args) {
 		SpringApplication.run(JdbcApplication.class, args);
 
 
